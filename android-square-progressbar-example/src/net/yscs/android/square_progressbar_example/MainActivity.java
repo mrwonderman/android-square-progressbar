a9b0469d4c84cd770b44bef612f516d3a8483414
Yannick Signer <thasiggi1@gmail.com>
2013-05-09 11:48:36 +0200

    Better design in the example app

--- android-square-progressbar-example/src/net/yscs/android/square_progressbar_example/MainActivity.java
+++ android-square-progressbar-example/src/net/yscs/android/square_progressbar_example/MainActivity.java
@@ -3,28 +3,24 @@ package net.yscs.android.square_progressbar_example;
 import net.yscs.android.square_progressbar.SquareProgressBar;
 import android.app.Activity;
 import android.os.Bundle;
-import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
 import android.widget.EditText;
 
 public class MainActivity extends Activity {
+	boolean darth = false;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 
-		final SquareProgressBar subi = (SquareProgressBar) findViewById(R.id.subi1);
-		subi.setImage(R.drawable.house);
-		subi.setProgress(25);
-
-		final SquareProgressBar subi2 = (SquareProgressBar) findViewById(R.id.subi2);
-		subi2.setImage(R.drawable.darth);
-		subi2.setProgress(3);
-		subi2.setColor(getApplicationContext().getResources().getColor(
-				android.R.color.holo_blue_dark));
+		final SquareProgressBar squareProgressBar = (SquareProgressBar) findViewById(R.id.subi2);
+		squareProgressBar.setImage(R.drawable.house);
+		squareProgressBar.setProgress(3);
+		squareProgressBar.setColor(getApplicationContext().getResources()
+				.getColor(android.R.color.holo_blue_dark));
 
 		Button button = (Button) findViewById(R.id.button1);
 		button.setOnClickListener(new OnClickListener() {
@@ -32,19 +28,25 @@ public class MainActivity extends Activity {
 			@Override
 			public void onClick(View v) {
 				EditText editText = (EditText) findViewById(R.id.editText1);
-				subi.setProgress(Integer
-						.parseInt(editText.getText().toString()));
-				subi2.setProgress(Integer.parseInt(editText.getText()
-						.toString()));
+				squareProgressBar.setProgress(Integer.parseInt(editText
+						.getText().toString()));
 			}
 		});
 
-	}
+		Button change = (Button) findViewById(R.id.button2);
+		change.setOnClickListener(new OnClickListener() {
 
-	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+			@Override
+			public void onClick(View v) {
+				if (darth) {
+					squareProgressBar.setImage(R.drawable.house);
+					darth = false;
+				} else {
+					squareProgressBar.setImage(R.drawable.darth);
+					darth = true;
+				}
+			}
+		});
 	}
 
 }
