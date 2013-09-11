b4b3429528849aad9d8c2832108b415fa3a2566f
Lucian <luksprog@gmail.com>
2013-09-11 09:05:04 +0300

    huge refactorring of the library

---
+++ android-square-progressbar/src/net/yscs/android/square_progressbar/BarRender.java
@@ -0,0 +1,149 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package net.yscs.android.square_progressbar;
+
+import android.graphics.Canvas;
+import android.graphics.Paint;
+import android.graphics.Paint.Style;
+import android.graphics.Rect;
+
+class BarRender {
+
+    private int mContainerWidth = 0;
+    private int mContainerHeight = 0;
+    private int mHalfWidth = 0;
+    private int mWidthSpace = 0;
+    private int mTotalRange;
+    private int mPixelStep = 0;
+    private int mMaxProgress;
+    private int mCurProgress;
+    private Paint mPaint = new Paint();
+    private Rect mTempRect = new Rect();
+    private int mExtraValue;
+
+    BarRender(int color) {
+        changeColor(color);
+        mPaint.setStyle(Style.FILL_AND_STROKE);
+        mPaint.setAntiAlias(true);
+    }
+
+    void setupValues(int width, int height, int space, int maxProgress) {
+        this.mContainerWidth = width;
+        this.mContainerHeight = height;
+        this.mWidthSpace = space;
+        mHalfWidth = mContainerWidth / 2;
+        mTotalRange = calculateEntireRange(width, height);
+        mPixelStep = calcPixelToProgressPoint(mTotalRange);
+    }
+
+    void changeColor(int color) {
+        mPaint.setColor(color);
+    }
+
+    void draw(Canvas canvas) {
+        final Rect r = mTempRect;
+        int totalPixelProgress = mCurProgress * mPixelStep;
+        int leftOver = totalPixelProgress - mHalfWidth;
+        if (leftOver <= 0) {
+            makeRectBar(r, mHalfWidth, 0, mHalfWidth + totalPixelProgress,
+                    mWidthSpace);
+            canvas.drawRect(r, mPaint);
+            return;
+        } else {
+            makeRectBar(r, mHalfWidth, 0, mContainerWidth, mWidthSpace);
+            canvas.drawRect(r, mPaint);
+        }
+        totalPixelProgress = leftOver + mExtraValue;
+        leftOver = totalPixelProgress - (mContainerHeight - mWidthSpace);
+        if (leftOver <= 0) {
+            makeRectBar(r, mContainerWidth - mWidthSpace, mWidthSpace,
+                    mContainerWidth, mWidthSpace + totalPixelProgress);
+            canvas.drawRect(r, mPaint);
+            return;
+        } else {
+            makeRectBar(r, mContainerWidth - mWidthSpace, mWidthSpace,
+                    mContainerWidth, mContainerHeight);
+            canvas.drawRect(r, mPaint);
+        }
+        totalPixelProgress = leftOver + mExtraValue;
+        leftOver = totalPixelProgress - (mContainerWidth - mWidthSpace);
+        if (leftOver <= 0) {
+            makeRectBar(r, mContainerWidth - totalPixelProgress - mWidthSpace,
+                    mContainerHeight - mWidthSpace, mContainerWidth
+                            - mWidthSpace, mContainerHeight);
+            canvas.drawRect(r, mPaint);
+            return;
+        } else {
+            makeRectBar(r, 0, mContainerHeight - mWidthSpace, mContainerWidth
+                    - mWidthSpace, mContainerHeight);
+            canvas.drawRect(r, mPaint);
+        }
+        totalPixelProgress = leftOver + mExtraValue;
+        leftOver = totalPixelProgress - (mContainerHeight - mWidthSpace);
+        if (leftOver <= 0) {
+            makeRectBar(r, 0, mContainerHeight - totalPixelProgress
+                    - mWidthSpace, mWidthSpace, mContainerHeight - mWidthSpace);
+            canvas.drawRect(r, mPaint);
+            return;
+        } else {
+            makeRectBar(r, 0, 0, mWidthSpace, mContainerHeight - mWidthSpace);
+            canvas.drawRect(r, mPaint);
+        }
+        totalPixelProgress = leftOver + mExtraValue;
+        if (mCurProgress == mMaxProgress) {
+            makeRectBar(r, mWidthSpace, 0, mHalfWidth, mWidthSpace);
+            canvas.drawRect(r, mPaint);
+        } else {
+            makeRectBar(r, mWidthSpace, 0, mWidthSpace + totalPixelProgress,
+                    mWidthSpace);
+            canvas.drawRect(r, mPaint);
+        }
+    }
+
+    void setProgress(int progress) {
+        mCurProgress = progress;
+    }
+
+    void setMaxProgress(int max) {
+        mMaxProgress = max;
+        mTotalRange = calculateEntireRange(mContainerWidth, mContainerHeight);
+        mPixelStep = calcPixelToProgressPoint(mTotalRange);
+    }
+
+    void setWidth(int newWidth) {
+        mWidthSpace = newWidth;
+    }
+
+    private void makeRectBar(Rect r, int left, int top, int right, int bottom) {
+        r.left = left;
+        r.top = top;
+        r.right = right;
+        r.bottom = bottom;
+    }
+
+    private int calculateEntireRange(int width, int height) {
+        int result = 2 * width + 2 * (height - 2 * mWidthSpace);
+        return result < 0 ? 0 : result;
+    }
+
+    private int calcPixelToProgressPoint(int entireRange) {
+        if (entireRange == 0 || mMaxProgress == 0) {
+            return 0;
+        }
+        int result = entireRange / mMaxProgress;
+        mExtraValue = (entireRange % mMaxProgress) / 4;
+        mExtraValue = mExtraValue == 0 ? 1 : mExtraValue;
+        return result;
+    }
+}
--- android-square-progressbar/src/net/yscs/android/square_progressbar/SquareProgressBar.java
+++ android-square-progressbar/src/net/yscs/android/square_progressbar/SquareProgressBar.java
@@ -1,227 +1,285 @@
 package net.yscs.android.square_progressbar;
 
-import net.yscs.android.square_progressbar.utils.CalculationUtil;
 import android.content.Context;
+import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.ColorMatrix;
 import android.graphics.ColorMatrixColorFilter;
 import android.util.AttributeSet;
-import android.view.LayoutInflater;
+import android.view.ViewGroup;
 import android.widget.ImageView;
-import android.widget.RelativeLayout;
 
 /**
- * The basic {@link SquareProgressBar}. THis class includes all the methods you
+ * The basic {@link SquareProgressBar}. This class includes all the methods you
  * need to modify your {@link SquareProgressBar}.
  * 
  * @author ysigner
  * @since 1.0
  */
-public class SquareProgressBar extends RelativeLayout {
+public class SquareProgressBar extends ViewGroup {
 
-	private ImageView imageView;
-	private final SquareProgressView bar;
-	private boolean opacity;
-	private boolean greyscale;
+    private ImageView mImage;
+    private boolean mOpacity;
+    private boolean mGreyscale;
+    private int mCurWidth;
+    private int mCurProgress;
+    private int mMaxProgress = 100;
+    private BarRender mRender;
 
-	/**
-	 * New SquareProgressBar.
-	 * 
-	 * @param context
-	 *            the {@link Context}
-	 * @param attrs
-	 *            an {@link AttributeSet}
-	 * @param defStyle
-	 *            a defined style.
-	 * @since 1.0
-	 */
-	public SquareProgressBar(Context context, AttributeSet attrs, int defStyle) {
-		super(context, attrs, defStyle);
-		LayoutInflater mInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mInflater.inflate(R.layout.progressbarview, this, true);
-		bar = (SquareProgressView) findViewById(R.id.squareProgressBar1);
-	}
+    /**
+     * New SquareProgressBar.
+     * 
+     * @param context
+     *            the {@link Context}
+     * @param attrs
+     *            an {@link AttributeSet}
+     * @param defStyle
+     *            a defined style.
+     * @since 1.0
+     */
+    public SquareProgressBar(Context context, AttributeSet attrs, int defStyle) {
+        super(context, attrs, defStyle);
+        init(context);
+    }
 
-	/**
-	 * New SquareProgressBar.
-	 * 
-	 * @param context
-	 *            the {@link Context}
-	 * @param attrs
-	 *            an {@link AttributeSet}
-	 * @since 1.0
-	 */
-	public SquareProgressBar(Context context, AttributeSet attrs) {
-		super(context, attrs);
-		LayoutInflater mInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mInflater.inflate(R.layout.progressbarview, this, true);
-		bar = (SquareProgressView) findViewById(R.id.squareProgressBar1);
-	}
+    /**
+     * New SquareProgressBar.
+     * 
+     * @param context
+     *            the {@link Context}
+     * @param attrs
+     *            an {@link AttributeSet}
+     * @since 1.0
+     */
+    public SquareProgressBar(Context context, AttributeSet attrs) {
+        super(context, attrs);
+        init(context);
+    }
 
-	/**
-	 * New SquareProgressBar.
-	 * 
-	 * @param context
-	 * @since 1.0
-	 */
-	public SquareProgressBar(Context context) {
-		super(context);
-		LayoutInflater mInflater = (LayoutInflater) context
-				.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-		mInflater.inflate(R.layout.progressbarview, this, true);
-		bar = (SquareProgressView) findViewById(R.id.squareProgressBar1);
-	}
+    /**
+     * New SquareProgressBar.
+     * 
+     * @param context
+     * @since 1.0
+     */
+    public SquareProgressBar(Context context) {
+        super(context);
+        init(context);
+    }
 
-	/**
-	 * Sets the image of the {@link SquareProgressBar}. Must be a valid
-	 * ressourceId.
-	 * 
-	 * @param image
-	 *            the image as a ressourceId
-	 * @since 1.0
-	 */
-	public void setImage(int image) {
-		imageView = (ImageView) findViewById(R.id.imageView1);
-		imageView.setImageResource(image);
+    private void init(Context context) {
+        mImage = new ImageView(context);
+        addView(mImage);
+        setWillNotDraw(false);
+        mRender = new BarRender(Color.YELLOW);
+        mRender.setWidth(0);
+        setMaximumProgress(100);
+    }
 
-	}
+    /**
+     * Sets the image of the {@link SquareProgressBar}. Must be a valid
+     * ressourceId.
+     * 
+     * @param image
+     *            the image as a ressourceId
+     * @since 1.0
+     */
+    public void setImage(int image) {
+        mImage.setImageResource(image);
+    }
 
-	/**
-	 * Sets the progress of the {@link SquareProgressBar}. If opacity is
-	 * selected then here it sets it. See {@link #setOpacity(boolean)} for more
-	 * information.
-	 * 
-	 * @param progress
-	 *            the progress
-	 * @since 1.0
-	 */
-	public void setProgress(double progress) {
-		bar.setProgress(progress);
-		if (opacity) {
-			setOpacity((int) progress);
-		} else {
-			setOpacity(100);
-		}
-	}
+    /**
+     * Sets the progress of the {@link SquareProgressBar}. If opacity is
+     * selected then here it sets it. See {@link #setOpacity(boolean)} for more
+     * information.
+     * 
+     * @param progress
+     *            the progress
+     * @since 1.0
+     */
+    public void setProgress(int progress) {
+        mCurProgress = progress;
+        mRender.setProgress(progress);
+        // if (mOpacity) {
+        // setOpacity((int) progress);
+        // } else {
+        // setOpacity(100);
+        // }
+        invalidate();
+    }
 
-	/**
-	 * Sets the colour of the {@link SquareProgressBar} to a predefined android
-	 * holo color. <br/>
-	 * <b>Examples:</b>
-	 * <ul>
-	 * <li>holo_blue_bright</li>
-	 * <li>holo_blue_dark</li>
-	 * <li>holo_blue_light</li>
-	 * <li>holo_green_dark</li>
-	 * <li>holo_green_light</li>
-	 * <li>holo_orange_dark</li>
-	 * <li>holo_orange_light</li>
-	 * <li>holo_purple</li>
-	 * <li>holo_red_dark</li>
-	 * <li>holo_red_light</li>
-	 * </ul>
-	 * 
-	 * @param androidHoloColor
-	 * @since 1.0
-	 */
-	public void setHoloColor(int androidHoloColor) {
-		bar.setColor(getContext().getResources().getColor(androidHoloColor));
-	}
+    public void setMaximumProgress(int maximumProgress) {
+        mMaxProgress = maximumProgress;
+        mRender.setMaxProgress(mMaxProgress);
+    }
 
-	/**
-	 * Sets the colour of the {@link SquareProgressBar}. YOu can give it a
-	 * hex-color string like <i>#C9C9C9</i>.
-	 * 
-	 * @param colorString
-	 *            the colour of the {@link SquareProgressBar}
-	 * @since 1.1
-	 */
-	public void setColor(String colorString) {
-		bar.setColor(Color.parseColor(colorString));
-	}
+    /**
+     * Sets the colour of the {@link SquareProgressBar} to a predefined android
+     * holo color. <br/>
+     * <b>Examples:</b>
+     * <ul>
+     * <li>holo_blue_bright</li>
+     * <li>holo_blue_dark</li>
+     * <li>holo_blue_light</li>
+     * <li>holo_green_dark</li>
+     * <li>holo_green_light</li>
+     * <li>holo_orange_dark</li>
+     * <li>holo_orange_light</li>
+     * <li>holo_purple</li>
+     * <li>holo_red_dark</li>
+     * <li>holo_red_light</li>
+     * </ul>
+     * 
+     * @param androidHoloColor
+     * @since 1.0
+     */
+    public void setHoloColor(int androidHoloColor) {
+        // bar.setColor(getContext().getResources().getColor(androidHoloColor));
+    }
 
-	/**
-	 * This sets the colour of the {@link SquareProgressBar} with a RGB colour.
-	 * 
-	 * @param r
-	 *            red
-	 * @param g
-	 *            green
-	 * @param b
-	 *            blue¨
-	 * @since 1.1
-	 */
-	public void setColorRGB(int r, int g, int b) {
-		bar.setColor(Color.rgb(r, g, b));
-	}
+    /**
+     * Sets the colour of the {@link SquareProgressBar}. YOu can give it a
+     * hex-color string like <i>#C9C9C9</i>.
+     * 
+     * @param colorString
+     *            the colour of the {@link SquareProgressBar}
+     * @since 1.1
+     */
+    public void setColor(String colorString) {
+        // bar.setColor(Color.parseColor(colorString));
+    }
 
-	/**
-	 * This sets the width of the {@link SquareProgressBar}.
-	 * 
-	 * @param width
-	 *            in Dp
-	 * @since 1.1
-	 */
-	public void setWidth(int width) {
-		int padding = CalculationUtil.convertDpToPx(width, getContext());
-		imageView.setPadding(padding, padding, padding, padding);
-		bar.setWidthInDp(width);
-	}
+    /**
+     * This sets the colour of the {@link SquareProgressBar} with a RGB colour.
+     * 
+     * @param r
+     *            red
+     * @param g
+     *            green
+     * @param b
+     *            blue¨
+     * @since 1.1
+     */
+    public void setColorRGB(int r, int g, int b) {
+        // bar.setColor(Color.rgb(r, g, b));
+    }
 
-	/**
-	 * This sets the alpha of the image in the view. Actually I need to use the
-	 * deprecated method here as the new one is only available for the API-level
-	 * 16. And the min API level o this library is 14.
-	 * 
-	 * Use this only as private method.
-	 * 
-	 * @param progress
-	 *            the progress
-	 */
-	private void setOpacity(int progress) {
-		imageView.setAlpha((int) (2.55 * progress));
-	}
+    /**
+     * This sets the width of the {@link SquareProgressBar}.
+     * 
+     * @param width
+     *            in Dp
+     * @since 1.1
+     */
+    public void setWidth(int newWidth) {
+        mCurWidth = newWidth;
+        mRender.setWidth(newWidth);
+        requestLayout();
+        invalidate();
+    }
 
-	/**
-	 * Switches the opacity state of the image. This forces the
-	 * SquareProgressBar to redraw with the current progress. As bigger the
-	 * progress is, then more of the image comes to view. If the progress is 0,
-	 * then you can't see the image at all. If the progress is 100, the image is
-	 * shown full.
-	 * 
-	 * @param opacity
-	 *            true if opacity should be enabled.
-	 */
-	public void setOpacity(boolean opacity) {
-		this.opacity = opacity;
-		setProgress(bar.getProgress());
-	}
+    /**
+     * This sets the alpha of the image in the view. Actually I need to use the
+     * deprecated method here as the new one is only available for the API-level
+     * 16. And the min API level o this library is 14.
+     * 
+     * Use this only as private method.
+     * 
+     * @param progress
+     *            the progress
+     */
+    private void setOpacity(int progress) {
+        mImage.setAlpha((int) (2.55 * progress));
+    }
 
-	/**
-	 * You can set the image to b/w with this method. Works fine with the
-	 * opacity.
-	 * 
-	 * @param greyscale
-	 *            true if the grayscale should be activated.
-	 */
-	public void setImageGrayscale(boolean greyscale) {
-		this.greyscale = greyscale;
-		if (greyscale) {
-			ColorMatrix matrix = new ColorMatrix();
-			matrix.setSaturation(0);
-			imageView.setColorFilter(new ColorMatrixColorFilter(matrix));
-		} else {
-			imageView.setColorFilter(null);
-		}
-	}
+    /**
+     * Switches the opacity state of the image. This forces the
+     * SquareProgressBar to redraw with the current progress. As bigger the
+     * progress is, then more of the image comes to view. If the progress is 0,
+     * then you can't see the image at all. If the progress is 100, the image is
+     * shown full.
+     * 
+     * @param opacity
+     *            true if opacity should be enabled.
+     */
+    public void setOpacity(boolean opacity) {
+        // TODO handle the opacity
+    }
 
-	public boolean isOpacity() {
-		return opacity;
-	}
+    /**
+     * You can set the image to b/w with this method. Works fine with the
+     * opacity.
+     * 
+     * @param greyscale
+     *            true if the grayscale should be activated.
+     */
+    public void setImageGrayscale(boolean greyscale) {
+        this.mGreyscale = greyscale;
+        if (greyscale) {
+            ColorMatrix matrix = new ColorMatrix();
+            matrix.setSaturation(0);
+            mImage.setColorFilter(new ColorMatrixColorFilter(matrix));
+        } else {
+            mImage.setColorFilter(null);
+        }
+    }
 
-	public boolean isGreyscale() {
-		return greyscale;
-	}
+    public boolean isOpacity() {
+        return mOpacity;
+    }
+
+    public boolean isGreyscale() {
+        return mGreyscale;
+    }
+
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        mImage.layout(mCurWidth, mCurWidth, getMeasuredWidth() - mCurWidth,
+                getMeasuredHeight() - mCurWidth);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+        final int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        final int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+        // first measure the ImageView with the current values(without the
+        // current mWidthSize) and see how bit it wants to be
+        mImage.measure(MeasureSpec.makeMeasureSpec(widthSize - 2 * mCurWidth,
+                widthMode), MeasureSpec.makeMeasureSpec(heightSize - 2
+                * mCurWidth, heightMode));
+        widthSize = Math.min(widthSize, mImage.getMeasuredWidth() + 2
+                * mCurWidth);
+        heightSize = Math.min(heightSize, mImage.getMeasuredHeight() + 2
+                * mCurWidth);
+        setMeasuredDimension(widthSize, heightSize);
+        mRender.setupValues(getMeasuredWidth(), getMeasuredHeight(), mCurWidth,
+                mMaxProgress);
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        canvas.drawColor(Color.GREEN);
+        super.onDraw(canvas);
+        mRender.draw(canvas);
+    }
+
+    @Override
+    protected void onSizeChanged(int w, int h, int oldw, int oldh) {
+        super.onSizeChanged(w, h, oldw, oldh);
+        mRender.setupValues(getMeasuredWidth(), getMeasuredHeight(), mCurWidth,
+                mMaxProgress);
+    }
+
+    @Override
+    public void setPadding(int left, int top, int right, int bottom) {
+        // TODO to implement!!!
+    }
+
+    @Override
+    public void setPaddingRelative(int start, int top, int end, int bottom) {
+        // TODO to implement!!!
+    }
+
 }
