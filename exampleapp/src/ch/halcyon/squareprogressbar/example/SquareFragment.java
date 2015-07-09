ba59856f742cec2022e2951fab319f4d12c37840
Yannick Signer <yannick@signer.pro>
2015-07-09 19:39:14 +0200

    changed default image
    changed default image to the new from blenheim palace

--- exampleapp/src/ch/halcyon/squareprogressbar/example/SquareFragment.java
+++ exampleapp/src/ch/halcyon/squareprogressbar/example/SquareFragment.java
@@ -18,7 +18,7 @@ public class SquareFragment extends Fragment {
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(ch.halcyon.squareprogressbar.example.R.layout.square_layout, container, false);
 		squareProgressBar = (SquareProgressBar) view.findViewById(ch.halcyon.squareprogressbar.example.R.id.subi2);
-		squareProgressBar.setImage(ch.halcyon.squareprogressbar.example.R.drawable.operahuset);
+		squareProgressBar.setImage(ch.halcyon.squareprogressbar.example.R.drawable.blenheim_palace);
 		squareProgressBar.setColor("#C9C9C9");
 		squareProgressBar.setProgress(32);
 		squareProgressBar.setWidth(8);
